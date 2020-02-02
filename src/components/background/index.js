7ef5819928b3c7203d86f578610454b041379b14
Haibin Zhu <ssspiderzzz@gmail.com>
2020-02-02 00:15:46 -0800

    full screen repeated ground bushs and sky

--- src/components/background/index.js
+++ src/components/background/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Background.css'
-import variables from './util'
 
 export default function Background (props) {
   return <div className='Background' style={{ color: 'blue' }} />
