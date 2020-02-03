a384a9d6a18e30d9eb46d77afd786e34f1d9a8f7
Haibin Zhu <ssspiderzzz@gmail.com>
2020-02-02 23:56:02 -0800

    add event listener

--- src/components/avatar/index.js
+++ src/components/avatar/index.js
@@ -1,8 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Avatar.css'
 import { actionX, actionY, actionRun } from '../util/variables.js'
 
 export default function Avatar (props) {
+  useEffect(() => {
+    window.addEventListener('keydown', downHandler)
+    window.addEventListener('keyup', upHandler)
+  }, [])
+
+  function downHandler (key) {
+    if (key.code === 'ArrowRight') {
+      setMovement({
+        ...movement,
+        left: movement.left + actionRun
+      })
+    }
+  }
+
+  function upHandler (key) {
+    console.log('key up', key)
+  }
+
   const [movement, setMovement] = useState({
     x: -480,
     y: -910,
