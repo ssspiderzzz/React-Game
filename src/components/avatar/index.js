67e7deb0773f8b38886f099dc3358ca93b8ea797
Haibin Zhu <ssspiderzzz@gmail.com>
2020-02-03 02:09:46 -0800

    add faceright and faceleft to control the movement face to

--- src/components/avatar/index.js
+++ src/components/avatar/index.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import './Avatar.css'
-import { actionX, actionY, actionRun } from '../util/variables.js'
+import { actionX, actionRun, faceRight, faceLeft } from '../util/variables.js'
 
 export default function Avatar (props) {
+  const [movement, setMovement] = useState({
+    x: faceRight.x,
+    y: faceRight.y,
+    moveOnXAxis: 0
+  })
+
+  let position = {
+    backgroundPositionX: movement.x + 'px',
+    backgroundPositionY: movement.y + 'px',
+    left: movement.moveOnXAxis + 'px'
+  }
+
   useEffect(() => {
     window.addEventListener('keydown', downHandler)
     window.addEventListener('keyup', upHandler)
@@ -10,26 +22,97 @@ export default function Avatar (props) {
 
   function downHandler (key) {
     if (key.code === 'ArrowRight') {
-      setMovement({
-        ...movement,
-        left: movement.left + actionRun
+      setMovement(prev => {
+        if (prev.y === faceLeft.y) {
+          return {
+            ...prev,
+            x: faceRight.x + actionX,
+            y: faceRight.y,
+            moveOnXAxis: prev.moveOnXAxis + actionRun
+          }
+        }
+        if (prev.moveOnXAxis < window.innerWidth + actionX) {
+          if (prev.x > actionX * 9) {
+            return {
+              ...prev,
+              x: prev.x + actionX,
+              moveOnXAxis: prev.moveOnXAxis + actionRun
+            }
+          } else {
+            return {
+              ...prev,
+              x: 0,
+              moveOnXAxis: prev.moveOnXAxis + actionRun
+            }
+          }
+        } else {
+          return { ...prev }
+        }
+      })
+    }
+
+    if (key.code === 'ArrowLeft') {
+      setMovement(prev => {
+        if (prev.y === faceRight.y) {
+          return {
+            ...prev,
+            x: faceLeft.x - actionX,
+            y: faceLeft.y,
+            moveOnXAxis: prev.moveOnXAxis - actionRun
+          }
+        }
+        if (prev.moveOnXAxis > 0) {
+          if (prev.x < 0) {
+            return {
+              ...prev,
+              x: prev.x - actionX,
+              moveOnXAxis: prev.moveOnXAxis - actionRun
+            }
+          } else {
+            return {
+              ...prev,
+              x: actionX * 9,
+              moveOnXAxis: prev.moveOnXAxis - actionRun
+            }
+          }
+        } else {
+          return { ...prev }
+        }
       })
     }
   }
 
   function upHandler (key) {
-    console.log('key up', key)
+    if (key.code === 'ArrowRight') {
+      clearTimeout()
+      setTimeout(() => {
+        setMovement(prev => {
+          return {
+            ...prev,
+            x: faceRight.x,
+            y: faceRight.y
+          }
+        })
+      }, 100)
+    }
+    if (key.code === 'ArrowLeft') {
+      clearTimeout()
+      setTimeout(() => {
+        setMovement(prev => {
+          return {
+            ...prev,
+            x: faceLeft.x,
+            y: faceLeft.y
+          }
+        })
+      }, 100)
+    }
   }
 
-  const [movement, setMovement] = useState({
-    x: -480,
-    y: -910,
-    left: 0
-  })
-  let position = {
-    backgroundPositionX: movement.x + 'px',
-    backgroundPositionY: movement.y + 'px',
-    left: movement.left + 'px'
-  }
-  return <div className='Avatar' style={position} />
+  return (
+    <>
+      {console.log(movement.moveOnXAxis)}
+      <div className='Avatar' style={position} />
+    </>
+  )
 }
--- src/components/util/variables.js
+++ src/components/util/variables.js
@@ -1,3 +1,5 @@
 export const actionX = -120
 export const actionY = -130
-export const actionRun = 10
+export const actionRun = 20
+export const faceRight = { x: -480, y: -910 }
+export const faceLeft = { x: -600, y: -650 }
