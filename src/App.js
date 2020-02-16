1f718b60680dcae7f531f2bde078c689a7819103
Haibin Zhu <ssspiderzzz@gmail.com>
2020-02-16 06:28:35 -0800

    remove non-used codes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
 import React, { useState, useEffect } from 'react'
+import Phaser from 'phaser'
 import { Router } from 'react-router-dom'
 import history from './history'
-import Background from './components/background'
-import Avatar from './components/avatar'
 import './App.css'
-import { faceRight } from './components/util/variables.js'
-import { downHandler, upHandler, control } from './components/control'
 
 export default function App (props) {
-  const [keys, setKeys] = useState({})
-  const [movement, setMovement] = useState({
-    x: faceRight.x,
-    y: faceRight.y,
-    moveOnXAxis: 0,
-    moveOnYAxis: 0
-  })
+  useEffect(() => {}, [])
+  var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 200 }
+      }
+    },
+    scene: {
+      preload: preload,
+      create: create
+    }
+  }
 
-  useEffect(() => {
-    window.addEventListener('keydown', key => downHandler(key, setKeys))
-    window.addEventListener('keyup', key =>
-      upHandler(key, setKeys, setMovement)
-    )
-  }, [])
+  var Game = new Phaser.Game(config)
 
-  useEffect(() => {
-    console.log(keys)
-    control(keys, setMovement)
-  }, [keys])
+  function preload () {
+    this.load.setBaseURL('http://labs.phaser.io')
+
+    this.load.image('sky', 'assets/skies/space3.png')
+    this.load.image('logo', 'assets/sprites/phaser3-logo.png')
+    this.load.image('red', 'assets/particles/red.png')
+    this.load.image('starBig', 'assets/games/starstruck/star2.png')
+  }
+
+  function create () {
+    this.add.image(400, 300, 'sky')
+
+    var particles = this.add.particles('red')
+
+    var emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: 'ADD'
+    })
+
+    var logo = this.physics.add.image(400, 100, 'starBig')
+
+    logo.setVelocity(100, 200)
+    logo.setBounce(1, 0.9)
+    logo.setCollideWorldBounds(true)
+
+    emitter.startFollow(logo)
+  }
 
   return (
     <div className='App'>
-      <Router history={history}>
-        <Background movement={movement} />
-        <Avatar movement={movement} setMovement={setMovement} />
-      </Router>
+      <Router history={history}></Router>
     </div>
   )
 }
