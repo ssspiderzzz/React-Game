ff13d1e0ff05b3f68fab70393a9a6c79e956379e
Haibin Zhu <ssspiderzzz@gmail.com>
2020-02-18 16:30:22 -0800

    fix coin count, add interactions with slime

--- src/App.js
+++ src/App.js
@@ -154,15 +154,15 @@ export default function App (props) {
     })
 
     // slime
-    this.slime = this.physics.add.sprite(500, 600, 'slime').setScale(4, 4)
+    this.slime = this.physics.add.sprite(500, 600, 'slime').setScale(2, 2)
     this.slime.body.collideWorldBounds = true
     this.anims.create({
       key: 'slime',
       frames: this.anims.generateFrameNumbers('slime', {
-        start: 20,
-        end: 29
+        start: 0,
+        end: 2
       }),
-      frameRate: 10,
+      frameRate: 5,
       repeat: -1
     })
     this.slime.anims.play('slime', true)
@@ -221,17 +221,32 @@ export default function App (props) {
     this.physics.add.collider(this.coins, this.player)
     this.physics.add.collider(this.slime, this.platforms)
     this.physics.add.collider(this.slime, this.player)
-    this.physics.add.collider(this.webs, this.platforms, event => {
-      // console.log(event)
-    })
+    this.physics.add.collider(this.webs, this.platforms)
     this.physics.add.overlap(this.webs, this.coins, (web, coin) => {
       this.money++
+      this.moneyChange = true
       coin.disableBody(true, true)
-      console.log(this.money)
+    })
+    this.physics.add.overlap(this.webs, this.slime, (web, slime) => {
+      this.slimeHP--
+      this.slime.body.x -= 2
     })
 
+    this.slimeHP = 100
     this.money = 0
+    this.moneyChange = false
     this.doublejump = false
+
+    // static text
+    this.collection = this.physics.add.staticGroup()
+    this.collection
+      .create(25, 25, 'coin', 0)
+      .setScale(0.3, 0.3)
+      .refreshBody()
+    this.collectionText = this.add.text(60, 7, this.money, {
+      fontFamily: '"Roboto Condensed"',
+      fontSize: 33
+    })
   }
 
   function update () {
@@ -280,6 +295,15 @@ export default function App (props) {
         )
         webShooter(newWeb_left, -350)
       }
+    }
+
+    if (this.moneyChange) {
+      this.moneyChange = false
+      this.collectionText.destroy()
+      this.collectionText = this.add.text(60, 7, this.money, {
+        fontFamily: '"Roboto Condensed"',
+        fontSize: 33
+      })
     }
   }
 
