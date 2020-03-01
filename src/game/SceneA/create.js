a720058a0eb5d74ebcc0977bb1e2f5182c7b2f87
Haibin Zhu <ssspiderzzz@gmail.com>
2020-02-29 21:38:56 -0800

    add characters' text on main menu

--- src/game/SceneA/create.js
+++ src/game/SceneA/create.js
@@ -12,18 +12,50 @@ export default function create () {
   let thor_face = this.add
     .image(1024 / 2 + 150, 300, 'thor_face')
     .setScale(2, 2)
+
   iron_man_face.setInteractive()
   captain_america_face.setInteractive()
   thor_face.setInteractive()
+
+  this.selectName = this.add.text()
+  iron_man_face.on('pointerdown', () => {
+    this.selectName.destroy()
+    this.selectName = this.add
+      .text(1024 / 2 - 150, 200, ['Iron Man', '(Tony Stark)'], {
+        fontSize: 22,
+        align: 'center'
+      })
+      .setOrigin(0.5)
+  })
+  captain_america_face.on('pointerdown', () => {
+    this.selectName.destroy()
+    this.selectName = this.add
+      .text(1024 / 2, 200, ['Captain America', '(Steve Rogers)'], {
+        fontSize: 22,
+        align: 'center'
+      })
+      .setOrigin(0.5)
+  })
+  thor_face.on('pointerdown', () => {
+    this.selectName.destroy()
+    this.selectName = this.add
+      .text(1024 / 2 + 150, 200, ['Thor', '(God of Thunder)'], {
+        fontSize: 22,
+        align: 'center'
+      })
+      .setOrigin(0.5)
+  })
 
   this.money = 0
   this.moneyChange = false
   this.doublejump = false
 
   // static text
-  let playButton = this.add.text(1024 / 2 - 35, 768 / 2, 'Play', {
-    fontSize: 33
-  })
+  let playButton = this.add
+    .text(1024 / 2, 768 / 2, 'Play', {
+      fontSize: 33
+    })
+    .setOrigin(0.5)
   playButton.setInteractive()
   playButton.on('pointerdown', () => {
     this.scene.start('SceneB')
