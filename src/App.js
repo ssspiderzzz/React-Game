b9b476d3ebdcfe61c179b0d04f844a4338e9b394
Haibin Zhu <ssspiderzzz@gmail.com>
2020-03-11 21:56:36 -0700

    init set up connecting to dynamoDB, use serverless API

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import { Router } from 'react-router-dom'
 import history from './history'
 import './App.css'
 import { config } from './game'
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import awsconfig from './aws-exports'
+import * as queries from './graphql/queries'
+// import * as mutations from './graphql/mutations';
+// import * as subscriptions from './graphql/subscriptions';
+
+Amplify.configure(awsconfig)
+async function fetch () {
+  const allTodos = await API.graphql(graphqlOperation(queries.listTodos))
+  console.log(allTodos)
+}
+fetch()
 
 export default function App (props) {
   new Phaser.Game(config)
 
   return (
     <div className='App'>
-      {/* <button
-        onClick={() => {
-          console.log(game)
-          game.restart()
-          console.log(game)
-        }}
-      >
-        Restart
-      </button> */}
       <Router history={history}></Router>
     </div>
   )
