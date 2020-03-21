b9530aabc81b154a0dbbec2419270900eb767f9e
Haibin Zhu <50757176+ssspiderzzz@users.noreply.github.com>
2020-03-21 16:57:02 -0700

    remove local config file for deployment

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import history from './history'
 import './App.css'
 import { config } from './game'
 import Amplify from 'aws-amplify'
-import awsconfig from './aws-exports'
 import UI from './components/UI.jsx'
 import { Provider } from 'react-redux'
 
 import store from './store'
 
 export default function App (props) {
-  Amplify.configure(awsconfig)
 
   let game = new Phaser.Game(config)
 
