322ad8b27d6509b16d58818f8cf9e5bfa5180b97
Haibin Zhu <ssspiderzzz@gmail.com>
2020-04-11 21:57:15 -0700

    update thanos skill

--- src/game/SceneB/update.js
+++ src/game/SceneB/update.js
@@ -480,30 +480,45 @@ export default async function update (time, delta) {
 }
 
 function thanosShooter (scene, thanos, direction) {
-  // shoot bullets
-  let plusX
-  let velocityX
-  let flipX
-  if (direction === 'right') {
-    plusX = 120
-    velocityX = 100
-    flipX = false
+  let velocityXRotation = [100, 75, 0, -75, -100, -75, 0, 75]
+  let velocityYRotation = [0, 75, 100, 75, 0, -75, -100, -75]
+  let rotateAngle = Math.PI / 4
+  for (let i = 0; i <= 7; i++) {
+    setTimeout(() => {
+      // shoot bullets
+      let plusX
+      let velocityX
+      let velocityY
+      let flipX
+      let rotation = rotateAngle * i
+
+      if (direction === 'right') {
+        plusX = 110
+        velocityX = velocityXRotation[i]
+        velocityY = velocityYRotation[i]
+        flipX = false
+      }
+      if (direction === 'left') {
+        plusX = 10
+        velocityX = -velocityXRotation[i]
+        velocityY = -velocityYRotation[i]
+        flipX = true
+      }
+      let newProjectile = scene.red_projectiles.create(
+        thanos.body.x + plusX,
+        thanos.body.y,
+        'red_projectiles'
+      )
+      newProjectile.setSize(90, 110, 0, 0).setOffset(35, 25)
+      // newProjectile.body.collideWorldBounds = true
+      newProjectile.body.allowGravity = false
+      newProjectile.flipX = flipX
+      newProjectile.anims.play('red_projectile', true)
+      newProjectile.body.velocity.x = velocityX
+      newProjectile.body.velocity.y = velocityY
+      newProjectile.setScale(0.3, 0.3)
+      newProjectile.setDepth(6)
+      newProjectile.rotation = rotation
+    }, 125 * i)
   }
-  if (direction === 'left') {
-    plusX = -20
-    velocityX = -100
-    flipX = true
-  }
-  let newProjectile = scene.red_projectiles.create(
-    thanos.body.x + plusX,
-    thanos.body.y,
-    'red_projectiles'
-  )
-  newProjectile.setSize(90, 110, 0, 0).setOffset(35, 25)
-  // newProjectile.body.collideWorldBounds = true
-  newProjectile.body.allowGravity = false
-  newProjectile.flipX = flipX
-  newProjectile.anims.play('red_projectile', true)
-  newProjectile.body.velocity.x = velocityX
-  newProjectile.setScale(0.5, 0.2)
 }
